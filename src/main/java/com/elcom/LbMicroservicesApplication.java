80d0e6216417b49a900e752f98c41e16827f34ba
Tranluc6622 <113016836+Tranluc6622@users.noreply.github.com>
2022-09-27 14:48:47 +0700

    ándlkasndlasndlaskdn

---
+++ src/main/java/com/elcom/LbMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.elcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbMicroservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elcom/LbMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbMicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
